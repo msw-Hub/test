870cdb605023806db093fb84726b0c713e10bf9d
msw-Hub <133941998+msw-Hub@users.noreply.github.com>
2024-02-19 23:41:05 +0900

    Add files via upload

---
+++ simple-board/src/main/java/com/example/simpleboard/post/db/PostEntity.java
@@ -0,0 +1,53 @@
+package com.example.simpleboard.post.db;
+
+
+import com.example.simpleboard.board.db.BoardEntity;
+import com.example.simpleboard.reply.db.ReplyEntity;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@ToString
+@Entity(name="post")
+public class PostEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JsonIgnore
+    @ToString.Exclude
+    @JoinColumn(name = "board_id")
+    private BoardEntity boardEntity; // board+ _id => board_id
+
+    private String userName;
+
+    private String password;
+
+    private String email;
+
+    private String status;
+
+    private String title;
+
+    @Column(columnDefinition = "TEXT")  //속성을 맞춰야 한다함.
+    private String content;
+
+    private LocalDateTime postedAt;
+
+    @OneToMany(
+            mappedBy = "post"
+    )
+    @Builder.Default
+    private List<ReplyEntity> replyList = new ArrayList<>();
+}
